c34ea043053b5dfc541bf410ccde85c2c2f90785
jiangzhaowei <jiangzhaowei2002@126.com>
2018-01-16 15:14:18 +0800

    VS2003 IDE测试

--- User.cpp
+++ User.cpp
@@ -29,7 +29,7 @@ void CLayers::Clear(void)
 
 void CLayers::Serialize(CArchive& ar)
 {
-	//CObject::Serialize(ar);
+	//CObject::Serialize(ar);//save is here
 	if (ar.IsStoring())
 	{
 		ar <<strPartNo<<strOutBox<<strInBox<<strDetail<<strUserCode<<iQty<<strOrder<<strNo<<iUnitWeight<<iRadio<<strPallet<<iUnitPrice<<strFactoryNo<<strUser1<<strUser2<<strUser3<<strUser4<<iUser1<<iUser2<<iUser3<<iUser4;
